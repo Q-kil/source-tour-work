82d76ec72b6e29b653cf765898f4463d08f79dff
Q-kil <kaifawebb@gmail.com>
2022-11-07 14:59:41 +0800

    The first lesson work done

--- packages/reactivity/src/index.ts
+++ packages/reactivity/src/index.ts
@@ -1,6 +1,6 @@
-//TODO: vue的响应式实现
-//@xiaoxiang/utils 本地实时获取
-import { isObject } from "@xiaoxiang/utils";
+// TODO: vue的响应式实现
+// @xiaoxiang/utils 本地实时获取
+import { isObject } from '@xiaoxiang/utils'
 
-let ret = isObject({})
-console.log('ret', ret);
+export const ret = isObject({})
+// console.log('ret', ret)
--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,9 +1,8 @@
-
-export function isObject(val:any){
-  return typeof val === 'object'
+export function isObject(val: any) {
+  return typeof val === 'object' && val !== null
 }
 
 // onClick
-export function isOn(key:string){
+export function isOn(key: string) {
   return key[0] === 'o' && key[1] === 'n'
 }
